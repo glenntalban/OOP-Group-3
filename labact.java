22ffbf39b20f3d9990ea60f083c4c41b104e0c9e
LANS <136078950+Lans-GB@users.noreply.github.com>
2025-10-06 11:21:07 +0800

    Update labact.java
    separated by module of functions

--- labact.java
+++ labact.java
@@ -4,105 +4,117 @@ import java.util.Random;
 
 public class EmployeeOrder {
 
-	public static void main(String[] args) {
-		System.out.println("Welcome to DNS Corporation!");
-		Scanner emp = new Scanner(System.in);
-	    Random random = new Random();
-		
-		
-		int empnum;
-		while (true) {
-			System.out.println("Enter the number of employees: ");
-			if (emp.hasNextInt() && (empnum = emp.nextInt()) > 0) break;
-			else {
-				System.out.println("Invalid input. Please enter a valid number.");
-				emp.next();s
-			}
-		}
-		
-		// this part saves the data
-		String[] name = new String[empnum];
-		int[] userID = new int[empnum];
-		String[] destination = new String[empnum];
-		int[] order_num = new int[empnum];
-		
-		for(int num = 1; num <= empnum; ++num) { // loop for entering details
-			// Employee details
-			System.out.println("Entering details for employee " + num);
-			
-			System.out.print("Enter employee name: ");
-			name[num-1] = emp.next();
-			
-			int id = checkID(emp, userID, num -1);
-			userID[num -1] = id;
-			
-			System.out.println("Enter travel order destination: ");
-			destination[num-1] = emp.next();
-			
-			// TRAVEL ORDER
-			order_num[num-1] = orderID(random);
-			
-			//TRAVEL DATE
-			System.out.println("Enter travel date (MM/DD/YYYY): ");
-			String travelDate = emp.next();
-			
-			//RETURN DATE
-			System.out.println("Enter return date (MM/DD/YYYY): ");
-			String returnDate = emp.next();
-			
-			
-			//EMPLOYEE PASS
-			String newcode = String.format("%05d", num);
-			String passnum = "EP" + newcode;
-			
-			// EMPLOYEE PASS OUTPUT
-			System.out.println();
-			System.out.println("Employee Details: ");
-			System.out.println("Name: " + name[num-1]);
-			System.out.println("ID: " + userID[num-1]);			
-			System.out.println("Employment Pass: " + passnum);
-			
-			//EMPLOYEE ORDER OUTPUT
-			System.out.println("");	
-			System.out.println("Travel Order Details: ");
-			System.out.println("Travel Date: " + travelDate + " Return Date: " + returnDate);
-			System.out.println("Destination: " + destination[num-1]);
-			System.out.println("Order Number: " + order_num[num-1]);
-			System.out.println("Employee: " + name[num-1]);
-			System.out.println();
-		}
-		System.out.println("Employee Order Successful");
-		}
-	//METHOD FOR CHECKING ID DUPLICATE PO HAHAHA
-	    private static int checkID(Scanner emp, int[] userID, int count) {
-	        while (true) {
-	            System.out.print("Enter employee ID: ");
-           	 			int userInf = emp.nextInt();
-           	 			if (userInf > 9999 && userInf < 99999) {
-		                boolean duplicate = false;
-		                for (int i = 0; i < count; i++) {
-		                    if (userID[i] == userInf) {
-		                        duplicate = true;
-		                        break;
-		                    }
-		                }
-		                if (!duplicate) {
-		                    return userInf; // valid, return it
-		                } else {
-		                    System.out.println("ID already used or invalid. Try another.");
-		                }
-		            } else {
-		                System.out.println("Invalid. Input valid ID.");
-		                emp.next(); // clear invalid input
-		                 
-		   	        }    
-	            }
-	        }
-      private static int orderID(Random random) {
-          int min = 1000;
-          int max = 10000; 
-          int randomNumber = random.nextInt(max - min) + min;
-		  return randomNumber; 
-    		  	}
-      }
-        	
+    public static void main(String[] args) {
+        System.out.println("Welcome to DNS Corporation!");
+        Scanner emp = new Scanner(System.in);
+        Random random = new Random();
+
+        int empnum = getEmployeeCount(emp);
+        
+        String[] name = new String[empnum];
+        int[] userID = new int[empnum];
+        String[] destination = new String[empnum];
+        int[] order_num = new int[empnum];
+
+        for (int num = 1; num <= empnum; ++num) {
+            System.out.println("Entering details for employee " + num);
+            
+            name[num - 1] = getEmployeeName(emp);
+            userID[num - 1] = getEmployeeID(emp, userID, num - 1);
+            destination[num - 1] = getDestination(emp);
+            order_num[num - 1] = generateOrderID(random);
+
+            String passnum = generatePassNumber(num);
+
+            displayEmployeeInfo(name[num - 1], userID[num - 1], passnum);
+            displayTravelOrderInfo(destination[num - 1], order_num[num - 1], name[num - 1]);
+        }
+
+        System.out.println("Employee Order Successful");
+    }
+
+    // ✅ Modular Functions
+
+    public static int getEmployeeCount(Scanner emp) {
+        int empnum;
+        while (true) {
+            System.out.println("Enter the number of employees: ");
+            if (emp.hasNextInt() && (empnum = emp.nextInt()) > 0) return empnum;
+            else {
+                System.out.println("Invalid input. Please enter a valid number.");
+                emp.next(); // clear invalid input
+            }
+        }
+    }
+
+    public static String getEmployeeName(Scanner emp) {
+        System.out.print("Enter employee name: ");
+        return emp.next();
+    }
+
+    public static int getEmployeeID(Scanner emp, int[] userID, int count) {
+        while (true) {
+            System.out.print("Enter employee ID (5-digit number): ");
+            if (!emp.hasNextInt()) {
+                System.out.println("Invalid input. Must be a number.");
+                emp.next(); // clear bad input
+                continue;
+            }
+
+            int userInf = emp.nextInt();
+
+            if (userInf < 10000 || userInf > 99999) {
+                System.out.println("Invalid ID. Must be a 5-digit number.");
+                continue;
+            }
+
+            // Check for duplicates
+            boolean duplicate = false;
+            for (int i = 0; i < count; i++) {
+                if (userID[i] == userInf) {
+                    duplicate = true;
+                    break;
+                }
+            }
+
+            if (duplicate) {
+                System.out.println("ID already used. Please enter a different ID.");
+            } else {
+                return userInf;
+            }
+        }
+    }
+
+    public static String getDestination(Scanner emp) {
+        System.out.println("Enter travel order destination: ");
+        return emp.next();
+    }
+
+    public static int generateOrderID(Random random) {
+        int min = 1000;
+        int max = 10000;
+        return random.nextInt(max - min) + min;
+    }
+
+    public static String generatePassNumber(int empIndex) {
+        String newcode = String.format("%05d", empIndex);
+        return "EP" + newcode;
+    }
+
+    public static void displayEmployeeInfo(String name, int id, String passnum) {
+        System.out.println();
+        System.out.println("Employee Details:");
+        System.out.println("Name: " + name);
+        System.out.println("ID: " + id);
+        System.out.println("Employment Pass: " + passnum);
+    }
+
+    public static void displayTravelOrderInfo(String destination, int orderNum, String empName) {
+        System.out.println();
+        System.out.println("Travel Order Details:");
+        System.out.println("Destination: " + destination);
+        System.out.println("Order Number: " + orderNum);
+        System.out.println("Employee: " + empName);
+        System.out.println();
+    }
+}
